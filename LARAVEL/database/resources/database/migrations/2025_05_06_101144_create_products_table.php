44aca04108afbceb4e2d3752937be6282114ac14
syandhilakarno <if23.syandhilakarno@mhs.ubpkarawang.ac.id>
2025-05-16 17:09:00 +0700

    Add files via upload

---
+++ LARAVEL/database/resources/database/migrations/2025_05_06_101144_create_products_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('brand')->nullable();
+            $table->enum('category', ['baju', 'celana', 'sepatu']);
+            $table->decimal('price', 10, 2);
+            $table->string('image_url');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
+
